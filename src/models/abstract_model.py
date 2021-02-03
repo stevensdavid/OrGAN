393b8978c16f600280ed7ccda44b4ba27fea33e2
David Stevens <dstevens@kth.se>
2021-02-03 15:01:39 +0100

    Change naming conventions from class to label

--- src/models/abstract_model.py
+++ src/models/abstract_model.py
@@ -5,12 +5,12 @@ from torch.utils.tensorboard import SummaryWriter
 
 class AbstractI2I(ABC):
     @abstractmethod
-    def transform(self, x: Tensor, c: Tensor) -> Tensor:
-        """Transform data to the target class
+    def transform(self, x: Tensor, y: Tensor) -> Tensor:
+        """Transform data to the target label
 
         Args:
             x (Tensor): [description]
-            c (Tensor): [description]
+            y (Tensor): [description]
 
         Returns:
             Tensor: [description]
@@ -21,18 +21,18 @@ class AbstractI2I(ABC):
     def loss(
         self,
         input_data: Tensor,
-        input_class: Tensor,
+        input_label: Tensor,
         output_data: Tensor,
-        output_class: Tensor,
+        output_label: Tensor,
         tensorboard: SummaryWriter = None,
     ) -> Tensor:
         """Calculate the loss of a transformation
 
         Args:
             input_data (Tensor): [description]
-            input_class (Tensor): [description]
+            input_label (Tensor): [description]
             output_data (Tensor): [description]
-            output_class (Tensor): [description]
+            output_label (Tensor): [description]
             tensorboard (SummaryWriter, optional): [description]. Defaults to None.
 
         Returns:
