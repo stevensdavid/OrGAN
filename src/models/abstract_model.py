039a9c19c4222646991525e00af31f0be6417311
David Stevens <dstevens@kth.se>
2021-02-04 01:16:30 +0100

    Change AbstractI2I loss to train_step

--- src/models/abstract_model.py
+++ src/models/abstract_model.py
@@ -5,35 +5,16 @@ from torch.utils.tensorboard import SummaryWriter
 
 class AbstractI2I(ABC):
     @abstractmethod
-    def transform(self, x: Tensor, y: Tensor) -> Tensor:
-        """Transform data to the target label
-
-        Args:
-            x (Tensor): [description]
-            y (Tensor): [description]
-
-        Returns:
-            Tensor: [description]
-        """
-        ...
-
-    @abstractmethod
-    def loss(
-        self,
-        input_data: Tensor,
-        input_label: Tensor,
-        output_data: Tensor,
-        output_label: Tensor,
-        tensorboard: SummaryWriter = None,
+    def train_step(
+        self, input_data: Tensor, input_label: Tensor, g_optimizer, d_optimizer,
     ) -> Tensor:
-        """Calculate the loss of a transformation
+        """[summary]
 
         Args:
             input_data (Tensor): [description]
             input_label (Tensor): [description]
-            output_data (Tensor): [description]
-            output_label (Tensor): [description]
-            tensorboard (SummaryWriter, optional): [description]. Defaults to None.
+            g_optimizer ([type]): [description]
+            d_optimizer ([type]): [description]
 
         Returns:
             Tensor: [description]
