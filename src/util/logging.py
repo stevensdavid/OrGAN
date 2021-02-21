54892b5668bc0d9181ef69a3044a237b748d2aae
David Stevens <dstevens@kth.se>
2021-02-21 23:35:03 +0100

    Serialize LossLogger to JSON

--- src/util/logging.py
+++ src/util/logging.py
@@ -1,6 +1,7 @@
 from torch.utils.tensorboard import SummaryWriter
 from dataclasses import asdict
-import pickle
+from pydoc import locate
+import json
 
 def add_key_prefix(dictionary: dict, prefix: str, separator: str = "/") -> dict:
     return {f"{prefix}{separator}{k}": v for k, v in dictionary.items()}
@@ -24,10 +25,18 @@ class LossLogger:
             self.generator_loss = 0
             
     def save(self, filepath: str):
-        with open(filepath, "wb") as f:
-            pickle.dump(self, f)
+        with open(filepath, "w") as f:
+            json.dump({
+                "generator_loss": asdict(self.generator_loss),
+                "discriminator_loss": asdict(self.discriminator_loss),
+                "steps": self.steps,
+                "generator_loss_type": type(self.generator_loss),
+                "discriminator_loss_type": type(self.discriminator_loss),
+            }, f)
 
-    @classmethod
-    def from_file(filepath: str):
-        with open(filepath, "rb") as f:
-            return pickle.load(f)
+    def restore(self, filepath: str):
+        with open(filepath, "r") as f:
+            checkpoint = json.load(f)
+        self.generator_loss = locate(checkpoint["generator_loss_type"])(**checkpoint["generator_loss"])
+        self.discriminator_loss = locate(checkpoint["discriminator_loss_type"])(**checkpoint["discriminator_loss"])
+        self.steps = checkpoint["steps"]
