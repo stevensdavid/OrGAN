a6ef7f3cca1a382ada778d5e838ba177a5899240
David Stevens <dstevens@kth.se>
2021-02-21 15:50:08 +0100

    Add functions for yaml dynamic object instantation

---
+++ src/util/object_loader.py
@@ -0,0 +1,30 @@
+import yaml
+from typing import Any
+from inspect import signature
+from pydoc import locate
+from typing import Type
+
+def load_yaml(filepath) -> dict:
+    with open(filepath) as file:
+        configuration = yaml.load(file, loader=yaml.FullLoader)
+    return configuration
+
+
+def build_from_yaml(filepath: str) -> Any:
+    config = load_yaml(filepath)
+    class_path: str = config["class_path"]
+    class_object: Type = locate(class_path)
+    try:
+        instance = class_object(**config.kwargs)
+    except TypeError:
+        sig = signature(class_object)
+        actual_kwargs = config.kwargs.keys()
+        expected_kwargs = sig.parameters.keys()
+        missing_kwargs = expected_kwargs - actual_kwargs
+        unexpected_kwargs = actual_kwargs - expected_kwargs
+        raise ValueError(
+            f"'kwargs' mapping in {filepath} contains errors.\n"
+            + f"Missing kwargs: {', '.join(missing_kwargs)}\n"
+            + f"Unexpected kwargs: {', '.join(unexpected_kwargs)}"
+        )
+    return instance
