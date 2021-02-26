5384c067433f9153c050f488797ee55e453007c7
David Stevens <dstevens@kth.se>
2021-02-26 20:31:35 +0100

    Move training config to YAML

--- src/main/train.py
+++ src/main/train.py
@@ -8,6 +8,8 @@ from data.abstract_classes import AbstractDataset
 from torch.utils.tensorboard import SummaryWriter
 from os import path
 import torch
+from util.dataclasses import TrainingConfig
+from util.enums import FrequencyMetric
 
 
 def parse_args() -> Namespace:
@@ -23,20 +25,13 @@ def parse_args() -> Namespace:
         "--model_config", type=str, help="Path to model YAML config", required=True
     )
     parser.add_argument(
-        "--checkpoint_frequency", type=int, help="How often to save model checkpoints"
+        "--train_config", type=str, help="Path to training YAML config", required=True
     )
     parser.add_argument(
         "--checkpoint_dir", type=str, help="Directory to save and load checkpoints from"
     )
     parser.add_argument("--resume_from", type=int)
-    parser.add_argument(
-        "--checkpoint_frequency_metric", type=str, choices=["epochs", "iterations"]
-    )
     parser.add_argument("--experiment_name", type=str, default="")
-    parser.add_argument("--log_frequency", type=int)
-    parser.add_argument(
-        "--log_frequency_metric", type=str, choices=["epochs", "iterations"]
-    )
     parser.add_argument("--batch_size", type=int)
     return parser.parse_args()
 
@@ -44,12 +39,15 @@ def parse_args() -> Namespace:
 def train(args: Namespace):
     dataset: AbstractDataset = build_from_yaml(args.data_config)
     model: AbstractI2I = build_from_yaml(args.model_config)
+    train_conf = TrainingConfig.from_yaml(args.train_config)
     discriminator_opt = Adam(model.discriminator_params())
     generator_opt = Adam(model.generator_params())
     tb = SummaryWriter(path.join(args.log_dir, args.experiment_name))
 
-    log_frequency = args.log_frequency * (
-        1 if args.log_frequency_metric == "iterations" else len(dataset)
+    log_frequency = train_conf.log_frequency * (
+        1
+        if train_conf.log_frequency_metric is FrequencyMetric.ITERATIONS
+        else len(dataset)
     )
     loss_logger = LossLogger(tb, log_frequency)
     if args.resume_from is not None:
@@ -60,8 +58,10 @@ def train(args: Namespace):
         discriminator_opt.load_state_dict(opt_state["d_opt"])
         model.load_checkpoint(args.resume_from)
 
-    checkpoint_frequency = args.checkpoint_frequency * (
-        1 if args.checkpoint_frequency_metric == "iterations" else len(dataset)
+    checkpoint_frequency = train_conf.checkpoint_frequency * (
+        1
+        if train_conf.checkpoint_frequency_metric is FrequencyMetric.ITERATIONS
+        else len(dataset)
     )
 
     g_scaler = GradScaler()
