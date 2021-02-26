da9126aad29dc07380d31bf63167f8a413dc6800
David Stevens <dstevens@kth.se>
2021-02-26 19:17:06 +0100

    Add stubs for toy experiment evaluation

--- src/main/eval.py
+++ src/main/eval.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser, Namespace
+from models.abstract_model import AbstractI2I
+from util.object_loader import build_from_yaml
 
 
 def parse_args() -> Namespace:
@@ -7,8 +9,25 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def eval(args: Namespace):
+def eval_toy_experiment(args: Namespace, model: AbstractI2I):
     pass
+
+
+def eval_angular(args: Namespace, model: AbstractI2I):
+    pass
+
+
+def eval_cartesian(args: Namespace, model: AbstractI2I):
+    pass
+
+
+def eval(args: Namespace):
+    model: AbstractI2I = build_from_yaml(args.model_config)
+    model.load_checkpoint(args.resume_from, args.checkpoint_dir)
+    if args.experiment == "angular_toy":
+        eval_angular(args, model)
+    elif args.experiment == "cartesian_toy":
+        eval_cartesian(args, model)
 
 
 if __name__ == "__main__":
