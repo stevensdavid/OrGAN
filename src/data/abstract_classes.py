b1cb785e289d48246160fa765691490d99d56805
David Stevens <dstevens@kth.se>
2021-02-26 19:15:25 +0100

    Add data split switching logic to AbstractDataset

--- src/data/abstract_classes.py
+++ src/data/abstract_classes.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from abc import ABC, abstractmethod
 from torch import tensor, Size
+from data.enums import DataSplit
 
 
 class AbstractDataset(Dataset, ABC):
@@ -9,4 +10,28 @@ class AbstractDataset(Dataset, ABC):
 
     @abstractmethod
     def random_targets(self, shape: Size) -> tensor:
+        ...
+
+    def set_mode(
+        self,
+        mode: DataSplit,
+    ) -> None:
+        self.mode = mode
+
+    def __len__(self) -> int:
+        if self.mode is None:
+            raise ValueError("Please call 'set_mode' before using data set")
+        return self._len()
+
+    def __getitem__(self, index: int):
+        if self.mode is None:
+            raise ValueError("Please call 'set_mode' before using data set")
+        return self._getitem(index)
+
+    @abstractmethod
+    def _len(self) -> int:
+        ...
+
+    @abstractmethod
+    def _getitem(self, index):
         ...
