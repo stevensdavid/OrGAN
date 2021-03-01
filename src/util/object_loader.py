0c65d046ab3e61a0711f40050e8036fd294f6a30
David Stevens <dstevens@kth.se>
2021-03-01 23:09:39 +0100

    Fix error logging for faulty kwargs

--- src/util/object_loader.py
+++ src/util/object_loader.py
@@ -7,7 +7,7 @@ from typing import Type
 
 def load_yaml(filepath) -> dict:
     with open(filepath) as file:
-        configuration = yaml.load(file, loader=yaml.FullLoader)
+        configuration = yaml.load(file, Loader=yaml.FullLoader)
     return configuration
 
 
@@ -17,15 +17,15 @@ def build_from_yaml(filepath: str, **kwargs) -> Any:
     build_kwargs = {**kwargs, **config["kwargs"]}
     try:
         instance = class_object(**build_kwargs)
-    except TypeError:
-        sig = signature(class_object)
-        actual_kwargs = config.kwargs.keys()
+    except TypeError as e:
+        sig = signature(class_object.__init__)
+        actual_kwargs = config["kwargs"].keys()
         expected_kwargs = sig.parameters.keys()
-        missing_kwargs = expected_kwargs - actual_kwargs
+        missing_kwargs = expected_kwargs - actual_kwargs - {"self"}
         unexpected_kwargs = actual_kwargs - expected_kwargs
         raise ValueError(
             f"'kwargs' mapping in {filepath} contains errors.\n"
             + f"Missing kwargs: {', '.join(missing_kwargs)}\n"
             + f"Unexpected kwargs: {', '.join(unexpected_kwargs)}"
-        )
+        ) from e
     return instance
